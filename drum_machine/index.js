bbcece3ef7f6d05aae53d5ab4aab51a60684a0ed
Brent <karmapoint@gmail.com>
2017-07-01 10:52:14 -0700

    add javascript to play sounds on keypress

---
+++ drum_machine/index.js
@@ -0,0 +1,24 @@
+
+
+
+function playSample(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(audio);
+  if(!audio) return; //stops
+  audio.currentTime = 0; //rewind each time
+  audio.play();
+  console.log(key);
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+  //this is key because addEventListener was called on key below
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSample);
+window.addEventListener('click', playSample);
