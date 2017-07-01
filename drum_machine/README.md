96126e2f89b118f2362df8c21d08c8df180d97ae
Brent <karmapoint@gmail.com>
2017-07-01 16:08:48 -0700

    add click to play samples

--- drum_machine/README.md
+++ drum_machine/README.md
@@ -11,3 +11,4 @@ I designed the logo and page as well as did the coding, extending the original t
     * Make large screen version with two 8 button rows
     * Make small screen version with smaller logo and now alpha-labels for the buttons
  * Add click/touch to play for the buttons
+ * Add a pre-loader so there is no delay on samples on first run
--- drum_machine/index.js
+++ drum_machine/index.js
@@ -11,6 +11,16 @@ function playSample(e) {
   key.classList.add('playing');
 }
 
+function playClickedSample(e) {
+  const identifier = e.target.getAttribute("data-key");
+  const audio = document.querySelector(`audio[data-key="${identifier}"]`);
+  const key = document.querySelector(`.key[data-key="${identifier}"]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
   this.classList.remove('playing');
@@ -19,6 +29,8 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('click', playClickedSample));
 
 window.addEventListener('keydown', playSample);
-window.addEventListener('click', playSample);
+
+// window.addEventListener('click', playClickedSample);
