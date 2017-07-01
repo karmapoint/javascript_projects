2432982759324720d32c52f399c67f2bf60ea33a
Brent <karmapoint@gmail.com>
2017-07-01 11:31:29 -0700

    improve styles

--- drum_machine/index.js
+++ drum_machine/index.js
@@ -1,14 +1,13 @@
 
 
-
 function playSample(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  console.log(audio);
+  //console.log(audio);
   if(!audio) return; //stops
   audio.currentTime = 0; //rewind each time
   audio.play();
-  console.log(key);
+  //console.log(key);
   key.classList.add('playing');
 }
 
@@ -20,5 +19,6 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
 window.addEventListener('keydown', playSample);
 window.addEventListener('click', playSample);
