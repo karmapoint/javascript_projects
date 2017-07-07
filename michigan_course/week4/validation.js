6aece00edefc88f61ba2f65685fb4e17a2697035
Brent <karmapoint@gmail.com>
2017-07-07 15:14:23 -0700

    complete week 4 homework

--- michigan_course/week4/validation.js
+++ michigan_course/week4/validation.js
@@ -8,3 +8,14 @@ function checkEmail() {
     return false;
   }
 }
+
+function nicknameFunction(){
+  if (document.getElementById("yesNick").checked){
+    document.getElementById("nick").style.display="inline";
+    document.getElementById("nickname").setAttribute('required', true);
+  }
+  else {
+    document.getElementById('nickname').removeAttribute("required");
+    document.getElementById('nick').style.display="none";
+  }
+}
