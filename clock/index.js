f97440fec80cf9fee3a9ff09c48fe787f32adf75
Brent <karmapoint@gmail.com>
2017-07-10 21:11:39 -0700

    init commit for clock

---
+++ clock/index.js
@@ -0,0 +1,12 @@
+
+
+// function getTime() {
+//   const secHand = document.getElementsByClassName('sec')[0];
+//   const now = new Date();
+//   const sec = now.getSeconds();
+//   const secDeg = ((sec / 60) * 360 );
+//   secHand.style.transform = `rotate(${secDeg} deg)`;
+//   console.log(secDeg);
+// }
+//
+// setInterval(getTime, 1000);
