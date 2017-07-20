cee698e791bd37fdd1f52690faa3317702669b2a
Brent <karmapoint@gmail.com>
2017-07-20 10:49:49 -0700

    make sound controls work

--- dice/index.js
+++ dice/index.js
@@ -1,8 +1,42 @@
+// SOUND CONTROLS
+
+let sound = true;
+
+let label = document.querySelector(".label");
+let box = document.querySelector(".switch");
+function toggleLabel(e) {
+  if (label.innerHTML === "Sound OFF") {
+      label.innerHTML = "Sound ON";
+      sound = true;
+    }
+    else {
+      label.innerHTML = "Sound OFF";
+      sound = false;
+    }
+}
+  box.addEventListener("change", toggleLabel, false);
+
+
+
+
+
+// Rolling the dice
+
 function rollDice(e){
   const audio = document.querySelector('#rollSound');
   audio.currentTime = 0;
-  audio.play();
+  if (sound) {
+    audio.play();
+  }
 }
+
+const rollButton = document.querySelector('#roll');
+rollButton.addEventListener('click', rollDice, false);
+
+
+
+
+// Toggle credit modals
 
 function toggleCredits(e){
   const settings = document.querySelector('.settings');
@@ -11,21 +45,24 @@ function toggleCredits(e){
   credits.classList.toggle('hidden');
 }
 
-function toggleSettings(e){
-  const credits = document.querySelector('.credits');
-  credits.classList.add('hidden');
-  const settings = document.querySelector('.settings');
-  settings.classList.toggle('hidden');
-}
-
-const rollButton = document.querySelector('#roll');
-rollButton.addEventListener('click', rollDice, false);
 
 const creditsButton = document.querySelector("#creditsBtn");
 creditsButton.addEventListener('click', toggleCredits, false);
 
 const creditsCloseButton = document.querySelector(".creditClose");
 creditsCloseButton.addEventListener('click', toggleCredits, false);
+
+
+
+
+// Toggle settings modals
+
+function toggleSettings(e){
+  const credits = document.querySelector('.credits');
+  credits.classList.add('hidden');
+  const settings = document.querySelector('.settings');
+  settings.classList.toggle('hidden');
+}
 
 const settingsButton = document.querySelector("#settingsBtn");
 settingsButton.addEventListener('click', toggleSettings, false);
