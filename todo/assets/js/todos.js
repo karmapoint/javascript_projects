bd56d054eac639ccb3dc1538ab8fae4a3c3dc8b9
Brent <karmapoint@gmail.com>
2017-08-07 15:58:59 -0700

    ability to add new items and attached listeners to new items

--- todo/assets/js/todos.js
+++ todo/assets/js/todos.js
@@ -1,13 +1,24 @@
 // Click todo to cross it off
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("done");
 });
 
 
 // Remove deleted items
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   event.stopPropagation();   //STOPS BUBBLE UP OF EVENT LISTENERS
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
+});
+
+
+// Add new Todos
+$('input[type="text"]').keypress(function(event){
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    $(this).val("");
+  }
+
 });
