41cddcffd6f1aa7da28b155bfaaf1ead908a9152
Brent <karmapoint@gmail.com>
2017-08-06 19:23:59 -0700

    add reset logic

--- color_picker/game.js
+++ color_picker/game.js
@@ -1,30 +1,49 @@
 let colors = generateRandomColors(6);
-console.log(colors);
 
 let squares = document.getElementsByClassName('square');
 let pickedColor = pickColor();
 let colorDisplay = document.querySelector("#targetColor");
-let messageBar = document.querySelector("#bar");
+let messageBar = document.querySelector(".middle");
+
+let resetButton = document.querySelector("#reset");
+
+
+/*  SETUP BOARD */
 colorDisplay.textContent = pickedColor;
 
+function setupBoard(){
+  let top = document.querySelector("header");
+  top.style.backgroundColor = "#333333";
+  messageBar.textContent = "";
+  
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
 
+    squares[i].addEventListener("click", function() {
+      let clickedColor = this.style.backgroundColor;
+      if (clickedColor === pickedColor) {
+        messageBar.textContent = "Correct";
+        changeColors(pickedColor);
 
-for (var i = 0; i < squares.length; i++) {
-  squares[i].style.backgroundColor = colors[i];
-
-  squares[i].addEventListener("click", function() {
-    let clickedColor = this.style.backgroundColor;
-    if (clickedColor === pickedColor) {
-      messageBar.textContent = "Correct";
-      changeColors(pickedColor);
-      let top = document.querySelector("header");
-      top.style.backgroundColor = pickedColor;
-    } else {
-      this.style.backgroundColor = "#333333";
-      messageBar.textContent = "Try again";
-    }
-  });
+        top.style.backgroundColor = pickedColor;
+      } else {
+        this.style.backgroundColor = "#333333";
+        messageBar.textContent = "Try again";
+      }
+    });
+  }
 }
+
+setupBoard();
+
+resetButton.addEventListener("click", function(){
+  colors = generateRandomColors(6);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  setupBoard();
+
+
+});
 
 
 function changeColors(color){
