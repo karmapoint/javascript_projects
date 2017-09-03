2705fcc1fa62b02e558fcf0c52d6d297eb959085
Brent <karmapoint@gmail.com>
2017-09-03 13:55:24 -0700

    add understanding directory

--- drum_machine/index.js
+++ drum_machine/index.js
@@ -1,37 +1,35 @@
-
-
 function playSample(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   //console.log(audio);
-  if(!audio) return; //stops
+  if (!audio) return; //stops
   audio.currentTime = 0; //rewind each time
   audio.play();
   //console.log(key);
-  key.classList.add('playing');
+  key.classList.add("playing");
 }
 
 function playClickedSample(e) {
   const identifier = e.target.getAttribute("data-key");
   const audio = document.querySelector(`audio[data-key="${identifier}"]`);
   const key = document.querySelector(`.key[data-key="${identifier}"]`);
-  if(!audio) return;
+  if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-  key.classList.add('playing');
+  key.classList.add("playing");
 }
 
 function removeTransition(e) {
-  if (e.propertyName !== 'transform') return;
-  this.classList.remove('playing');
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
   //this is key because addEventListener was called on key below
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-keys.forEach(key => key.addEventListener( 'click', playClickedSample));
-keys.forEach(key => key.addEventListener( 'touchstart', playClickedSample));
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+keys.forEach(key => key.addEventListener("click", playClickedSample));
+keys.forEach(key => key.addEventListener("touchstart", playClickedSample));
 
-window.addEventListener('keydown', playSample);
+window.addEventListener("keydown", playSample);
 
 // window.addEventListener('click', playClickedSample);
